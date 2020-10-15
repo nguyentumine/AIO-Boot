49a4cba24e657db8d011ef3e4fb941c6bd0117bb
ademkocamaz <kocamazadem@gmail.com>
2020-10-15 11:40:28 +0300

    Update Manually
    If updating issues occur, you can use update.bat to update AIOBoot.

---
+++ update/update.bat
@@ -0,0 +1,5 @@
+@echo off
+mode 100,4
+curl -sL https://api.github.com/repos/nguyentumine/AIO-Boot/releases/latest | jq -r ".assets[].browser_download_url" > url.txt
+wget -q -O AIOBoot.7z --show-progress -i url.txt
+7zG.exe  x -y -o"..\" -paioboot AIOBoot.7z
